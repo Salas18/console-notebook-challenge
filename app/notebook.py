a33c2fdeab769f9312395067a0e31e9139fd4a77
MIGUEL ANGEL <migueangelso13@gmail.com>
2025-02-18 14:28:27 -0500

    creacion clase notebook y sus atributos

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,19 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+import datetime
+from datetime import datetime
+
+class Notebook:
+    HIGH: str = 'HIGH'
+    MEDIUM: str = 'MEDIUM'
+    LOW: str = 'LOW'
+
+    def __init__(self, code, title, text, importance, tags, creation_date):
+        self.code: str = code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.tags: list[str] = []
+        self.creation_date = datetime
+
+
+
