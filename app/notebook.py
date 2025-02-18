fcd141b8cf5bcb32c67fd1cfc6519af7db8d5e2d
MIGUEL ANGEL <migueangelso13@gmail.com>
2025-02-18 14:29:39 -0500

    creacion de metodos

--- app/notebook.py
+++ app/notebook.py
@@ -15,5 +15,35 @@ class Notebook:
         self.tags: list[str] = []
         self.creation_date = datetime
 
+        def crear_titulo(self, titulo, ) -> str:
+            return f"el titulo es {self.titulo}"
+
+        def nota(self, texto) -> str:
+            return f'el texto dice {self.texto}'
+
+        def importancia_nota(self, HIGH, MEDIUM, LOW, identificador) -> str:
+            if identificador == HIGH:
+                return f'la importancia de la nota es HIGH'
+            if identificador == MEDIUM:
+                return f'la importancia de la nota es MEDIUM'
+            else:
+                return f"la importancia de la nota es LOW"
+
+        def add_tag(self, tag: str):
+            for tag in self.tags:
+                if tag is not self.tags:
+                    tag.append(self.tags)
+                else:
+                    return f'ya esta agregada'
+
+        def listar_notas(self):
+            pass
+
+        def datetime(self, hora, formato_fecha_hora):
+            hora = datetime.now()
+            formato_fecha_hora = hora.strftime("%Y-%m-%d %H:%M:%S")
+            return formato_fecha_hora
+
+
 
 
